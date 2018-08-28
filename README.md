8175b0d1bd6842fb2f1e814f99ad96035e50e734
Philip McGrath <philip@philipmcgrath.com>
2018-08-28 11:57:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+libxml2-x86_64-linux-natipkg
+====================
+A platform-specific Racket package providing libxml2 
+for the package-build server.
+
+The current version is 2.9.3 for consistency with `xmllint-win32-x86_64`.
+
+The binary `libxml2.so.2.9.3` was compiled from the sources at: 
+ftp://xmlsoft.org/libxml2/libxml2-2.9.3.tar.gz
---
+++ info.rkt
@@ -0,0 +1,11 @@
+#lang info
+(define collection "libxml2-x86_64-linux-natipkg")
+(define deps '("base"))
+(define build-deps '())
+(define pkg-desc "Provides libxml2 for the package-build server")
+(define version "0")
+(define pkg-authors '(philip))
+
+(define install-platform "x86_64-linux-natipkg")
+(define copy-foreign-libs '("libxml2.so.2.9.3"))
+
