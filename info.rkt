db8c462fcf098b2590558f71125ce33dec11d72c
Bogdan Popa <bogdan@defn.io>
2023-03-10 09:12:41 +0200

    build: fix version declaration

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"))
 (define build-deps '())
 (define pkg-desc "Provides libxml2 for the package-build server")
-(define version "0")
+(define version "0.0")
 (define pkg-authors '(philip))
 
 (define install-platform "x86_64-linux-natipkg")
